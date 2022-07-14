1b9d6ba788be8952e8d197f274ded3b0c9bd6d82
azizovenemy <104134689+azizovenemy@users.noreply.github.com>
2022-07-15 00:27:17 +0300

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Space]
+    [SerializeField] private float speed;
+    [SerializeField] private float sensitivity;
+    [SerializeField] private float jumpForce;
+    
+    private Vector3 playerMovementInput;
+    private Vector2 playerMouseInput;
+    private Rigidbody playerBody;
+    private float rotation;
+    private Transform playerCamera;
+
+    private void Start()
+    {
+        playerBody = GetComponent<Rigidbody>();
+        playerCamera = FindObjectOfType<Camera>().transform;
+    }
+
+    private void FixedUpdate()
+    {
+        playerMovementInput = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+        playerMouseInput = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+        
+        MovePlayer();
+        MovePlayerCamera();
+    }
+
+    private void MovePlayer()
+    {
+        Vector3 MoveVector = transform.TransformDirection(playerMovementInput) * speed;
+        playerBody.velocity = new Vector3(MoveVector.x, playerBody.velocity.y, MoveVector.z);
+    }
+
+    private void MovePlayerCamera()
+    {
+        rotation -= playerMouseInput.y * sensitivity;
+
+        gameObject.transform.Rotate(0f, playerMouseInput.x * sensitivity, 0f);
+        playerCamera.transform.localRotation = Quaternion.Euler(rotation, 0f, 0f);
+    }
+}
